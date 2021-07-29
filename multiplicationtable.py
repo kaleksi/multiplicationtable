2ae0ac6faa4807b8a585b8351dd73b4cd28e488a
Kaleb <mammallama9@gmail.com>
2021-07-29 01:13:43 -0400

    Initial commit

---
+++ multiplicationtable.py
@@ -0,0 +1,25 @@
+
+def multi_table(number):
+    result = number * 1
+    print("1 * " + str(number) + " = " + str(result))
+    result = number * 2
+    print("2 * " + str(number) + " = " + str(result))
+    result = number * 3
+    print("3 * " + str(number) + " = " + str(result))
+    result = number * 4
+    print("4 * " + str(number) + " = " + str(result))
+    result = number * 5
+    print("5 * " + str(number) + " = " + str(result))
+    result = number * 6
+    print("6 * " + str(number) + " = " + str(result))
+    result = number * 7
+    print("7 * " + str(number) + " = " + str(result))
+    result = number * 8
+    print("8 * " + str(number) + " = " + str(result))
+    result = number * 9
+    print("9 * " + str(number) + " = " + str(result))
+    result = number * 10
+    print("10 * " + str(number) + " = " + str(result))
+
+#number you'd like to create a table for goes here
+multi_table(300)
